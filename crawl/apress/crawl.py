6ddd944e665dba120d559faa649ea4589d4083af
Liang-Chieh Chen <luniacslime@gmail.com>
2015-06-04 02:33:27 +0900

    wait for discussion of stroage

--- crawl/apress/crawl.py
+++ crawl/apress/crawl.py
@@ -18,31 +18,48 @@ def get(url):
     return requests.get(url).text;
 
 class apress_parse:
+    host = 'http://www.apress.com/';
     
-    pre_url = 'http://www.apress.com/';
     def __init__(self):
         #parse all category
-        sleep(0);
+        res = get(self.host);
+        soup = BeautifulSoup(res);
+        ca = [];
+        for ul in soup.find_all('ul', {'id': 'nav'}):
+            for a in ul.find_all('a'):
+                ca.append(a.get('href').strip());
+        for link in ca:
+            self.parse_category(link);
 
-    def parse_book(self, category):
+    def parse_category(self, link):
         page = 1;
         while 1:
-            res = get(self.pre_url + category + '?p=' + str(page));
+            res = get(link + '?p=' + str(page));
             soup = BeautifulSoup(res);
             for ul in soup.find_all('ul', 'products-grid-cust-cat'):
                 for li in ul.find_all('li'):
                     url = li.find_all('a')[0].get('href').strip();
-                    print url;                    
+                    self.parse_book(url);                   
                     
             page += 1;
-            if self.end(soup):
+            if self.end_category(soup):
                 break;
 
-    def end(self, soup):
+    def end_category(self, soup):
         for p in soup.find_all('p', 'amount'):
             a = re.findall('Books \d+-(\d+?) of (\d+)', p.get_text());
-            print a[0][0] + ' ' + a[0][1];
             return a[0][0] == a[0][1];
+
+    def parse_book(self, link):
+        res = get(link);
+        soup = BeautifulSoup(res);
+        
+        #title source link type toc
+        book = {};
+        book['title'] = soup.find_all('h1')[0].get_text().strip();        book['source'] = 'apress';
+        book['link'] = link;
+        book['type'] = 'book';
+        
 
 if __name__ == '__main__':
     hi = apress_parse();
