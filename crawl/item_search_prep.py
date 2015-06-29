cc81fefad322be3c990fd09a5c8c78715aeb3fe5
suhorng <suhorng@gmail.com>
2015-06-30 02:10:18 +0800

    fix n-gram generating; TOCs shouldn't have been concatenated

--- crawl/item_search_prep.py
+++ crawl/item_search_prep.py
@@ -3,6 +3,7 @@
 import json
 import stoplist
 
+GRAM_MAX = 4
 PUNCTUATIONS = stoplist.PUNCTUATIONS.split()
 STOP_WORDS = set(stoplist.STOP_WORDS.split())
 
@@ -11,17 +12,17 @@ def simpl_stopwords_split(s):
     s = s.replace(punc, "")
   return [w for w in s.split() if not (w in STOP_WORDS or w.isdigit())]
 
+def accum_ngrams(n, words): # compute i-gram for i in xrange(n)
+  return [" ".join(ws) for i in xrange(n) \
+                       for ws in zip(*(words[j:] for j in xrange(i+1)))]
+
 res = []
 for fil in ["apress", "oreilly-data-id.json", "mit.json"]:
   with open(fil, "r") as filp:
     data = json.load(filp)
   for item in data:
-    words = [w for sec in item[u"toc"] for w in simpl_stopwords_split(sec.lower())]
     # TODO: stemming and stopwords
-    words = words \
-          + [" ".join(ws) for ws in zip(words, words[1:])] \
-          + [" ".join(ws) for ws in zip(words, words[1:], words[2:])] \
-          + [" ".join(ws) for ws in zip(words, words[1:], words[2:], words[3:])]
+    words = [w for sec in item[u"toc"] for w in accum_ngrams(GRAM_MAX, simpl_stopwords_split(sec.lower()))]
     words = list(set(words))
     words.sort()
     res.append({u"id": item[u"id"], u"vocabs": words})
