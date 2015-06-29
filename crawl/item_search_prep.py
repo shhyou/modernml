9f3869876db3a9844ecca7303fee5338c36debb1
suhorng <suhorng@gmail.com>
2015-06-29 14:31:19 +0800

    Search bi-grams in keywords

--- crawl/item_search_prep.py
+++ crawl/item_search_prep.py
@@ -8,9 +8,13 @@ for fil in ["apress", "oreilly-data-id.json"]:
   res = []
   for item in data:
     words = [w for sec in item[u"toc"] for w in sec.split()]
-    words = [w.replace("'s", "") for w in words]
+    words = [w.replace("'s", "").replace(",", "") for w in words]
     words = [w.lower() for w in words]
     words = [w for w in words if w != u"&"]
+    # TODO: stemming and stopwords
+    words = words \
+          + [" ".join(ws) for ws in zip(words, words[1:])] \
+          + [" ".join(ws) for ws in zip(words, words[1:], words[2:])]
     words = list(set(words))
     words.sort()
     res.append({u"id": item[u"id"], u"vocabs": words})
