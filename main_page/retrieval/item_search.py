a53ce108177d9b7b846cacfbf166b9d1b86e5914
suhorng <suhorng@gmail.com>
2015-06-29 13:30:17 +0800

    revise keyword search; interpret keywords conjunctively and preprocess vocabs

--- main_page/retrieval/item_search.py
+++ main_page/retrieval/item_search.py
@@ -2,29 +2,35 @@ import configs
 
 import json
 
+with open(configs.ITEM_VOCABS, "r") as filp:
+  item_vocabs = [{u"id": vocab[u"id"], u"vocabs": set(vocab[u"vocabs"]) } \
+                 for vocab in json.load(filp)]
+
 def search(keywords):
   """Search related books/courses from the keywords
 
-  keywords: A list of string. Search keywords;
+  keywords: A list of string. Search keywords; the keywords are used as 'conjuction'
   returns: A list of integers representing IDs of related items
   """
 
   # Possibly TODO: normalize keywords (stopwords & stemming & cases)
-  ids = []
-  for fil in configs.FILE_LIST:
-    with open(fil, "r") as filp:
-      data = json.load(filp)
-    for item in data:
-      if any([section for section in item[u"toc"] if keyword in section] \
-             for keyword in keywords):
-        ids.append(item[u"id"])
-  ids.sort()
-  return list(set(ids))
+  keywords = [keyword.lower().replace("'s", "") for keyword in keywords]
+
+  itsets = []
+  for i, keyword in enumerate(keywords):
+    itsets.append([])
+    for vocab in item_vocabs:
+      if keyword in vocab["vocabs"]:
+        itsets[i].append(vocab[u"id"])
+    itsets[i] = set(itsets[i])
+
+  ids = itsets[0]
+  for idlist in itsets:
+    ids &= idlist
+  return list(ids)
 
 if __name__ == "__main__":
-  print "search_book test?"
-  print "Python\n", search([u"Python"])
+  print "item_search test"
   print "python\n", search([u"python"])
   print "\nmachine learning"
-  print search([u"machine learning"])
   print search([u"machine", u"learning"])
