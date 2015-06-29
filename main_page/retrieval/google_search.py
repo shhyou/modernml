e46b7404c1a2d3b71573d6b1626b85e08f524097
Liang-Chieh Chen <luniacslime@gmail.com>
2015-06-29 19:49:39 +0900

    fixed bug

--- main_page/retrieval/google_search.py
+++ main_page/retrieval/google_search.py
@@ -6,13 +6,19 @@ def search(keyword):
 	res = requests.get("https://www.google.com.tw/search?q=site:en.wikipedia.org+" + keyword + "&bav=on.2,or.&cad=b&fp=8d02688dbcaec00c&biw=808&bih=667&dpr=1&tch=1&ech=1&psi=1d6QVe-XHs798AW5kqnABg.1435557589508.3")
 	content = res.text.split('/*""*/')[1]
 	dic = json.loads(content)
-	links = re.findall('/url\?q=https://en.wikipedia.org/wiki/(.+?)"', dic['d'])
 	results = []
+	links = re.findall('/url\?q=https://en.wikipedia.org/%3Ftitle%3D(.+?)"|/url\?q=https://en.wikipedia.org/wiki/(.+?)"', dic['d'])
 	for link in links:
+		if link[0] == '':
+			prefix = 'https://en.wikipedia.org/wiki/'
+			link = link[1]
+		else:
+			prefix = 'https://en.wikipedia.org/?title='
+			link = link[0]
 		if link.find("%23") == -1:
 			keyword = link.split("&amp;")[0]
-			results.append('https://en.wikipedia.org/wiki/' + keyword)
+			results.append(prefix + keyword)
 	return results
 
 if __name__ == '__main__':
-	print search('test')
+	print search('heapsort')
