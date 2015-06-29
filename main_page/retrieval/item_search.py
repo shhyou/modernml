333925ce986e572c7dba7789d422337e29879ea8
suhorng <suhorng@gmail.com>
2015-06-29 12:22:02 +0800

    add naive keyword searching

--- main_page/retrieval/item_search.py
+++ main_page/retrieval/item_search.py
@@ -1,14 +1,30 @@
 import configs
 
+import json
+
 def search(keywords):
   """Search related books/courses from the keywords
 
   keywords: A list of string. Search keywords;
   returns: A list of integers representing IDs of related items
   """
-  print configs.FILE_LIST
-  raise NotImplementedError
+
+  # Possibly TODO: normalize keywords (stopwords & stemming & cases)
+  ids = []
+  for fil in configs.FILE_LIST:
+    with open(fil, "r") as filp:
+      data = json.load(filp)
+    for item in data:
+      if any([section for section in item[u"toc"] if keyword in section] \
+             for keyword in keywords):
+        ids.append(item[u"id"])
+  ids.sort()
+  return list(set(ids))
 
 if __name__ == "__main__":
   print "search_book test?"
-  pass
+  print "Python\n", search([u"Python"])
+  print "python\n", search([u"python"])
+  print "\nmachine learning"
+  print search([u"machine learning"])
+  print search([u"machine", u"learning"])
